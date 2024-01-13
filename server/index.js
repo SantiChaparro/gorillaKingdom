2073c9ff5be1c8b2e7addd82c14b90128da32089
santiago <santiago@chaparro.com.ar>
2024-01-12 21:54:00 -0300

    segundo commit

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 const server = require('./src/server');
+const { conn } = require('./src/db');
 require('dotenv').config();
 //const PORT = 3001;
 const PORT = process.env.PORT
 
-server.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`);
-})
+conn.sync({force: true}).then(async ()=>{
+
+    server.listen(PORT,()=>{
+        console.log(`Listening to port ${PORT}`);
+    })    
+
+}).catch(error => console.error(error))
+
+
 
